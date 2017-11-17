c55439d905b635b196c3154374a2f6e23afac621
Gauthier Dieppedalle <gauthier.dieppedalle@gmail.com>
2017-11-16 22:04:13 -0800

    Added new login screen designs.

--- Assets/Menu_Buttons.cs
+++ Assets/Menu_Buttons.cs
@@ -9,6 +9,7 @@ public class Menu_Buttons : MonoBehaviour {
 	public GameObject LevelSelectPanel;
 	public GameObject LoginPanel;
 	public GameObject SignUpPanel;
+	Vector3 endPos;
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +17,31 @@ public class Menu_Buttons : MonoBehaviour {
 		LevelSelectPanel.SetActive(false);
 		LoginPanel.SetActive(false);
 		SignUpPanel.SetActive(false);
+		/*GameObject buttonA = GameObject.Find("logo");
+
+		Vector3 pos = buttonA.transform.position;
+		Vector3 endPos = buttonA.transform.position;
+		endPos.y += 300f;
+		buttonA.transform.position = Vector3.MoveTowards(pos, endPos, 1f * Time.deltaTime);*/
+		//buttonA.transform.position = Vector3.Lerp(pos, endPos, 10 * Time.deltaTime);
+		//buttonA.transform.position = pos;
+
+		//float elapsedTime = 0;
+		//Vector3 startingPos = buttonA.transform.position;
+		GameObject buttonA = GameObject.Find("logo");
+		endPos = buttonA.transform.position;
+		endPos.y += 300f;
+		/*while (elapsedTime < 3f)
+		{
+			transform.position = Vector3.Lerp(startingPos, endPos, (elapsedTime / 3f));
+			elapsedTime += Time.deltaTime;
+			//yield return new WaitForEndOfFrame();
+		}
+		transform.position = end;*/
+		Debug.Log (buttonA.transform.position);
+		buttonA.transform.position = Vector3.Lerp(buttonA.transform.position ,endPos, 1000f);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
